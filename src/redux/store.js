9e4d9c9028b5c44ac22f95fb776d744d8052e6ae
ahmedaziz22-tech <ahmed.aziz4465@gmail.com>
2025-11-15 17:53:56 +0200

    -

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist"; // ⚠️ ADDED
-import AsyncStorage from "@react-native-async-storage/async-storage"; // ⚠️ ADDED
-import { combineReducers } from "redux"; // ⚠️ ADDED
+import { persistStore, persistReducer } from "redux-persist"; 
+import AsyncStorage from "@react-native-async-storage/async-storage"; 
+import { combineReducers } from "redux"; 
 import productsReducer from "./slices/productSlice";
 import cartReducer from "./slices/cartSlice";
 import authReducer from "./slices/authSlice";
 
-// ⚠️ ADDED: Persist Config
+
+
+
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
-  whitelist: ["auth", "cart"], // Only persist auth and cart
+  whitelist: ["auth", "cart"], 
 };
 
-// ⚠️ ADDED: Combine Reducers
+
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
   auth: authReducer,
 });
 
-// ⚠️ ADDED: Persisted Reducer
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// ⚠️ MODIFIED: Store with persisted reducer
+
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
@@ -34,7 +36,7 @@ const store = configureStore({
     }),
 });
 
-// ⚠️ ADDED: Persistor
+
 export const persistor = persistStore(store);
 
 export default store;
